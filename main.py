3df537a1b933c97a38993ad303c2aa17b9a6bd72
ching-yi-hsu <kup1125@outlook.com>
2020-11-09 22:37:39 +0800

    show all files

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+from os import listdir
+from os.path import isfile, isdir, join
 import os
 
-print(os.getcwd())
+
+mypath = os.getcwd()
+
+files = listdir(mypath)
+
+for f in files:
+    print(f)
+
